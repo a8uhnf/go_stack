e4cdb4f8dd9dafa4b6469ce0ab4eec756daac4b5
hanif <hanif@appscode.com>
2017-10-11 11:16:46 +0600

    go test

--- test/hello.go
+++ test/hello.go
@@ -2,6 +2,22 @@ package test
 
 import "fmt"
 
+type Book struct {
+	Title string
+	Author string
+	Pages int64
+}
+
+func (ko Book) CategoryByLength() string {
+	var ret string
+	if ko.Pages > 300 {
+		ret = "NOVEL"
+	} else {
+		ret = "SHORT STORY"
+	}
+	return ret
+}
+
 func hello(s string) (string) {
 	return fmt.Sprintf("Hello, %s", s)
 }
